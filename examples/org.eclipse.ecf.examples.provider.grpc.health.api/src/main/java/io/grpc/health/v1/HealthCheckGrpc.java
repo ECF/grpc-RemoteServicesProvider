cd11c08daf3e68f727d6a8393c5f729b7a3ad902
scottslewis <slewis@composent.com>
2021-10-14 18:14:14 -0700

    Updated metadata to include rx examples.

--- examples/org.eclipse.ecf.examples.provider.grpc.health.api/src/main/java/io/grpc/health/v1/HealthCheckGrpc.java
+++ examples/org.eclipse.ecf.examples.provider.grpc.health.api/src/main/java/io/grpc/health/v1/HealthCheckGrpc.java
@@ -5,7 +5,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
 /**
  */
 @javax.annotation.Generated(
-    value = "by gRPC proto compiler (version 1.36.1)",
+    value = "by gRPC proto compiler (version 1.39.0)",
     comments = "Source: health.proto")
 public final class HealthCheckGrpc {
 
